640f092f024bdb9a189571372ef95a8ee57f30b0
MdShadil07 <mdshadil62@gmail.com>
2025-01-11 01:05:37 +0530

    All file updatae

--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,12 +1,5 @@
-function countChar(str, char) {
-//   str = str.split(" ").join("");
-//   SameSen = str.toLowerCase();
-//   char = char.toLowerCase();
-//   let count = 0;
-//   for (i = 0; i < SameSen.length; i++) {
-//     if (SameSen[i] === char.toLowerCase()) {
-//       count++;
-//     }
-//   }
-//   return count;
-// }
+console.log(checkTriangleType(3, 3, 3)); //Equilateral
+
+// console.log(checkTriangleType(3, 4, 5)); // isosceles
+
+// console.log(checkTriangleType(5, 8, 6)); // scalene
